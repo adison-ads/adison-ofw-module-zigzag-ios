efb21e3b67be0d9aa66b547a6bc5684bc6b755d2
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-04-30 16:40:47 +0900

    패키지 설정

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZigzagOfwModule",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ZigzagOfwModule",
+            targets: ["ZigzagOfwModule", "ZigzagOfwModuleTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: "3.6.0"),
+        .package(url: "https://github.com/airbnb/lottie-spm", from: "4.4.2")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "ZigzagOfwModule",
+            url: "https://github.com/adison-ads/adison-ofw-module-zigzag-ios/releases/download/0.10.8/ZigzagOfwModule.zip",
+            checksum: ""
+        ),
+        .target(
+            name: "ZigzagOfwModuleTarget",
+            dependencies: [
+                .product(name: "AdisonOfferwallSDK", package: "adison-offerwall-ios-sdk"),
+                .product(name: "Lottie", package: "lottie-spm")
+            ],
+            path: "Sources"
+        )
+    ]
+)
