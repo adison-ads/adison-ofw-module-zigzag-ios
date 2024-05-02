4c316780ea1ffb19f38f13553b6c5935eec71607
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-05-02 11:34:14 +0900

    0.10.18 릴리즈

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "ZigzagOfwModule",
-            url: "https://github.com/adison-ads/adison-ofw-module-zigzag-ios/releases/download/0.10.16/ZigzagOfwModule.zip",
-            checksum: "ec8badff05c9db751df4a02722e76a5e4fda286fbcb0d5a7576b838a2215a8e1"
+            url: "https://github.com/adison-ads/adison-ofw-module-zigzag-ios/releases/download/0.10.18/ZigzagOfwModule.zip",
+            checksum: "263928c46e24fd4909f12a277cc4e5c4fa453cadd9801511e1be57eb780327b1"
         ),
         .target(
             name: "ZigzagOfwModuleTarget",
