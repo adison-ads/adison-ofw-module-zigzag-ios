53281c78f87000f726070a2f1adb1f240c22d1c5
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2025-01-20 15:58:14 +0900

    1.4.2 릴리즈

--- Package.swift
+++ Package.swift
@@ -15,15 +15,15 @@ let package = Package(
             targets: ["ZigzagOfwModule", "ZigzagOfwModuleTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: "3.9.2")
+        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: "3.9.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "ZigzagOfwModule",
-            url: "https://github.com/adison-ads/adison-ofw-module-zigzag-ios/releases/download/1.4.0/ZigzagOfwModule.zip",
-            checksum: "7127a30e1b4a3055129df5c3e8fac3a4ce4d8c76f72781b99bb21ddee446774b"
+            url: "https://github.com/adison-ads/adison-ofw-module-zigzag-ios/releases/download/1.4.2/ZigzagOfwModule.zip",
+            checksum: "d972318e01e73d40f3fef692e86aaff93b6e06632a5a63630677ee9fe96b47a0"
         ),
         .target(
             name: "ZigzagOfwModuleTarget",
